e6cc4645b0f957ef8f75443c7a54602cb7bc317e
YashJha26 <142669810+YashJha26@users.noreply.github.com>
2024-10-22 10:27:18 +0530

    Update authHandler.js, header is sent now

--- src/api/authHandler.js
+++ src/api/authHandler.js
@@ -3,10 +3,29 @@ const BASE_URL = import.meta.env.VITE_BASE_URL;
 export default async function authFetchHandler({ endPoint, method = "GET", data }) {
   const url = `${BASE_URL}/${endPoint}`;
   //console.log("authHandler",data);
+  let allCookies = document.cookie;
+  console.log(allCookies);
+
+// Function to get a specific cookie by name
+function getCookie(name) {
+    let cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+        let cookie = cookies[i].trim();
+        if (cookie.startsWith(name + '=')) {
+            return cookie.substring((name + '=').length);
+        }
+    }
+    return null;
+}
+
+// Usage
+  let token = getCookie("token");
+  const headers = token ? { Authorization: `Bearer ${token}` } : {};
   const options = {
     withCredentials: true,
     method,
     data,
+    headers,
   };
 
   try {
